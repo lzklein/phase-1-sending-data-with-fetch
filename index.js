1ae6e61fca3a7865b2b9c62eaa7a165dd0526f55
Louis Klein <louisklein71@gmail.com>
2023-05-12 10:19:03 -0700

    tes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData (name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(r => r.json())
+    .then(data => {
+        const body = document.querySelector('body');
+        body.append(data.id);
+    })
+    .catch(function (error){
+        const body = document.querySelector('body');
+        body.append(error);
+    })
+}
